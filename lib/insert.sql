ff31023a954ec62b1913fba6ff32258b33fd3f14
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-17 22:39:19 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears VALUES (
+    1, "Mr Chocolate", 45, "M", "Brown & Black", "Aggresive", 1
+);
+INSERT INTO bears VALUES (
+    2, "Rowdy", 33, "M", "Black", "Peaceful", 1
+);
+INSERT INTO bears VALUES (
+    3, "Tabitha", 12, "F", "Brown", "Angry", 0 
+);
+INSERT INTO bears VALUES (
+    4, "Sergeant Brown", 24, "F", "Brown", "Calm", 1
+);
+INSERT INTO bears VALUES (
+    5, "Melissa", 32, "F", "White", "Aggresive", 1
+);
+INSERT INTO bears VALUES (
+    6, "Grinch", 12, "Male", "Brown", "Grumpy", 1
+);
+INSERT INTO bears VALUES (
+    7, "Wendy", 29, "F", "Black", "Friendly", 0
+);
+INSERT INTO bears VALUES (
+    8, null, 29, "M", "Black", "Angry", 0
+);
